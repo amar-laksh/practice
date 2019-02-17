9b71978c696314a193867b01f59af4bef05b64f9
amar.lakshya <amar.lakshya@xaviers.edu.in>
2019-02-17 23:01:59 +0530

    FIXED: sorting.h

--- include/sorting.h
+++ include/sorting.h
@@ -1,8 +1,69 @@
 #ifndef SORTING
 #define SORTING 1
+#include <stdio.h>
+enum algorithm {
+    SELECTION,
+    INSERTION,
+    MERGE
+};
+struct sorting_t {
+    void (*print)(int* a);
+    int* (*sort)(int a[], enum algorithm algo);
+    enum algorithm algo;
+};
 
-int* selection_sort(int a[]);
-int* insertion_sort(int a[]);
-int* merge_sort(int a[]);
+int* selection_sort(int a[])
+{
+    int key = 0, j = 0;
+    for (int i = 0; i < 6; i++) {
+	key = a[i];
+	j = i - 1;
+	while (j >= 0 && a[j] > key) {
+	    a[j + 1] = a[j];
+	    j -= 1;
+	}
+	a[j + 1] = key;
+    }
+    return a;
+}
+
+int* insertion_sort(int a[])
+{
+    return a;
+}
+
+int* merge_sort(int a[])
+{
+    return a;
+}
+
+int* sort_fn(int a[], enum algorithm algo)
+{
+    switch (algo) {
+    case SELECTION:
+	return selection_sort(a);
+    case INSERTION:
+	return insertion_sort(a);
+    case MERGE:
+	return merge_sort(a);
+    default:
+	printf("Sorry please choose from the available algorithms\n");
+    }
+    return 0;
+}
+
+void print_fn(int* a)
+{
+    printf("%d", a[0]);
+    for (int i = 1; i < 6; ++i) {
+	printf(", %d", *(a + i));
+    }
+    printf("\n");
+}
+
+struct sorting_t Sort = {
+    .print = print_fn,
+    .sort = sort_fn
+};
 
 #endif /* ifndef SORTING */
--- practice.c
+++ practice.c
@@ -3,45 +3,6 @@
 
 #define PROJECT_NAME "practice"
 
-enum algorithm {
-    SELECTION,
-    INSERTION,
-    MERGE
-};
-
-struct sorting {
-    void (*print)(int* a);
-    int* (*sort)(int a[], enum algorithm);
-    enum algorithm algo;
-};
-
-void print_fn(int* a)
-{
-    printf("%d", a[0]);
-    for (int i = 1; i < 6; ++i) {
-	printf(", %d", *(a + i));
-    }
-    printf("\n");
-}
-int* sort_fn(int a[], enum algorithm algo)
-{
-    switch (algo) {
-    case SELECTION:
-	return selection_sort(a);
-    case INSERTION:
-	return insertion_sort(a);
-    case MERGE:
-	return merge_sort(a);
-    default:
-	printf("Sorry please choose from the available algorithms\n");
-    }
-    return 0;
-}
-
-struct sorting Sort = {
-    .print = print_fn,
-    .sort = sort_fn
-};
 int main(int argc, char** argv)
 {
     if (argc != 1) {
@@ -50,7 +11,7 @@ int main(int argc, char** argv)
     }
     int arr[] = { 5, 2, 4, 6, 1, 3 };
     printf("Original array: ");
-    print_fn(arr);
+    Sort.print(arr);
     printf("\n");
     printf("INSERTION sort array: ");
     Sort.print(Sort.sort(arr, INSERTION));
